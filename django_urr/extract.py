2e0d9cb746d9c3e10b0cd75316a1433557cfafb9
Aarni Koskela <akx@iki.fi>
2026-03-05 18:42:19 +0200

    Add types

--- django_urr/extract.py
+++ django_urr/extract.py
@@ -1,70 +1,73 @@
-import itertools
+from __future__ import annotations
 
-from django import urls
+import itertools
+import re
+from collections.abc import Iterator
+from typing import Union
+
+from django.urls import URLPattern, URLResolver
+from django.urls.resolvers import URLResolver as _URLResolver
 from django.utils.functional import cached_property
 from django.utils.regex_helper import normalize
 
-try:  # Django 2.0+
-    url_resolver_types = (urls.URLResolver,)
-    DJANGO_2 = True
-except AttributeError:  # Django 1.11
-    url_resolver_types = (urls.RegexURLResolver,)
-    DJANGO_2 = False
+URLBit = Union[URLResolver, URLPattern]  # noqa: UP007
 
 
 class URLEntry:
-    def __init__(self, url_bits):
-        self.bits = url_bits[:]
-        self.name = url_bits[-1].name
+    def __init__(self, url_bits: list[URLBit]) -> None:
+        self.bits: list[URLBit] = url_bits[:]
+        last = url_bits[-1]
+        self.name: str | None = last.name if isinstance(last, URLPattern) else None
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         if self.qualified_name:
             return f"<URLEntry {self.merged_pattern!r} (name: {self.qualified_name!r})>"
         return f"<URLEntry {self.merged_pattern!r}>"
 
-    def normalize(self):
+    def normalize(self) -> list[tuple[str, list[str]]]:
         return normalize(self.merged_pattern)
 
     @cached_property
-    def namespace(self):
-        return getattr(self.bits[0], "namespace", None)
+    def namespace(self) -> str | None:
+        first = self.bits[0]
+        if isinstance(first, _URLResolver):
+            return first.namespace
+        return None
 
     @cached_property
-    def qualified_name(self):
+    def qualified_name(self) -> str | None:
         if self.name and self.namespace:
             return f"{self.namespace}:{self.name}"
         return self.name
 
     @cached_property
-    def regexes(self):
-        if DJANGO_2:
-            return [bit.pattern.regex for bit in self.bits]
-        return [bit.regex for bit in self.bits]
+    def regexes(self) -> list[re.Pattern[str]]:
+        return [bit.pattern.regex for bit in self.bits]
 
     @cached_property
-    def merged_pattern(self):
-        return "".join(re.pattern.lstrip("^").rstrip("$") for re in self.regexes)
+    def merged_pattern(self) -> str:
+        return "".join(r.pattern.lstrip("^").rstrip("$") for r in self.regexes)
 
     @cached_property
-    def named_groups(self):
-        keys = (re.groupindex.keys() for re in self.regexes)
+    def named_groups(self) -> set[str]:
+        keys = (r.groupindex.keys() for r in self.regexes)
         return set(itertools.chain(*keys))
 
     @cached_property
-    def group_count(self):
-        return sum(re.groups for re in self.regexes)
+    def group_count(self) -> int:
+        return sum(r.groups for r in self.regexes)
 
 
-def _extract_urls(urlpatterns, parents):
+def _extract_urls(urlpatterns: list[URLBit], parents: list[URLBit]) -> Iterator[URLEntry]:
     for pattern in urlpatterns:
         path = parents[:] + [pattern]
-        if isinstance(pattern, url_resolver_types):
+        if isinstance(pattern, _URLResolver):
             yield from _extract_urls(pattern.url_patterns, path)
         else:
             yield URLEntry(path)
 
 
-def extract_urls(urlpatterns=None):
+def extract_urls(urlpatterns: list[URLBit] | None = None) -> Iterator[URLEntry]:
     """
     Extract URLEntry objects from the given iterable
     of Django URL pattern objects.  If no iterable is given,
@@ -76,5 +79,7 @@ def extract_urls(urlpatterns=None):
     :rtype: list[URLEntry]
     """
     if urlpatterns is None:
-        urlpatterns = urls.get_resolver(None).url_patterns
+        from django.urls import get_resolver
+
+        urlpatterns = get_resolver(None).url_patterns
     yield from _extract_urls(urlpatterns, [])
